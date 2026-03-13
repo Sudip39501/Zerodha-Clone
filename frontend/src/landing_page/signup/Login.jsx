67def9e32ede90be0d9a857a170cea15254ba636
Sudip Biswas <sudipabc17@gmail.com>
2026-03-13 21:52:03 +0530

    frontend deploy

--- frontend/src/landing_page/signup/Login.jsx
+++ frontend/src/landing_page/signup/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const data = await axios.post(
-        "http://localhost:3000/login",
+        "https://zerodha-clone-kuuf.onrender.com/login",
         {
           ...inputValue,
         },
--- frontend/src/landing_page/signup/Signup.jsx
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/signup",
+        "https://zerodha-clone-kuuf.onrender.com/signup",
         {
           ...inputValue,
         },
