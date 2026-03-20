94b1837afa061b09c5060e513c13e6daf57f6464
Sudip Biswas <sudipabc17@gmail.com>
2026-03-20 11:59:05 +0530

    add useEffect

--- dashboard/src/component/Home.jsx
+++ dashboard/src/component/Home.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
 import withAuth from "../Utils/withAuth";
 
 const Home = () => {
+  useEffect(() => {
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get("token");
+
+  if (token) {
+    localStorage.setItem("token", token); // save to localStorage
+    window.history.replaceState({}, "", "/"); // ✅ immediately clean URL
+  }
+}, []);
+
+
   return (
     <div className="h-screen">
       <TopBar />
