ae9a08a5fc5c5747321434c120d0cc1d82f37f4a
Sudip Biswas <sudipabc17@gmail.com>
2026-03-20 13:34:31 +0530

    final fix

--- dashboard/src/Utils/withAuth.jsx
+++ dashboard/src/Utils/withAuth.jsx
@@ -3,13 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 const withAuth = (WrappedComponent) => {
   const AuthComponent = (props) => {
-    const params = new URLSearchParams(window.location.search);
-    const token = params.get("token");
+    
 
     useEffect(() => {
       console.log(token);
-      // if(token)
-      if (!token) {
+      let isToken = lolocalStorage.getItem("token") ? true : false;
+      if (!isToken) {
         window.location.href =
           "https://zerodha-clone-ten-weld.vercel.app/OpenAccount/login";
       }
